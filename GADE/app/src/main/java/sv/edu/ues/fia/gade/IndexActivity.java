90c41d0989b4f1def074fc259266dff8a74c88b7
Christian254 <christianfuentes254@gmail.com>
2018-05-01 21:54:56 -0600

    Usuario Normal
    Activities

--- GADE/app/src/main/java/sv/edu/ues/fia/gade/IndexActivity.java
+++ GADE/app/src/main/java/sv/edu/ues/fia/gade/IndexActivity.java
@@ -1,14 +1,38 @@
 package sv.edu.ues.fia.gade;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class IndexActivity extends AppCompatActivity {
+
+public class IndexActivity extends ListActivity {
+    String[] menu= {"Solicitar Local", "Consultar Reservas","Modificar Reserva", "Eliminar Reservas"};
+    String[] valores={"SolicitarLocalActivity", "ConsultarReservaActivity","ModificarReservaActivity", "EliminarReservaActivity"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_index);
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu));
+    }
+
+    @Override
+    protected  void  onListItemClick(ListView listView, View view, int position, long id)
+    {
+        String nombreValue=valores[position];
+        try {
+            Class<?> clase = Class.forName("sv.edu.ues.fia.gade.UsuarioNormal."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
---
+++ GADE/app/src/main/java/sv/edu/ues/fia/gade/UsuarioNormal/ModificarReservaActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.gade.UsuarioNormal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import sv.edu.ues.fia.gade.R;
+
+public class ModificarReservaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_reserva);
+    }
+}
